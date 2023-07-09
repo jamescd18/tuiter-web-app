661f4fbe7bfb96f3375a47a088d21e43682d6ac1
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-07-09 17:17:01 -0400

    Complete A1 Lab

---
+++ app/labs/a1/other-page/page.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function LabA1OtherPage() {
+  return (
+    <>
+      <Link href="../a1#anchor-tag">Go Back</Link>
+      <h1>Other Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam
+        doloremque eos exercitationem ipsa voluptate eaque consequatur eius
+        quasi possimus, adipisci necessitatibus? Cum doloremque architecto rem,
+        dolor voluptates amet ipsam consequuntur eaque excepturi error animi quo
+        modi facere alias fugit pariatur veritatis atque! Earum itaque nostrum
+        ut consequatur, perferendis illum dignissimos ea, vitae fugiat labore ab
+        debitis doloribus dicta corrupti. Rem aut optio quaerat eius quidem
+        nihil assumenda, harum modi commodi deserunt sequi vitae recusandae
+        dolor, porro ea ad facilis nostrum consectetur laudantium doloribus.
+        Dolore iure dolorum perferendis, temporibus sapiente saepe excepturi
+        veniam quaerat maiores quos officiis exercitationem. Tempora a
+        temporibus quos aperiam ipsam maiores modi consequatur sequi non ipsum,
+        sit eaque enim reprehenderit velit minus nihil magnam laborum unde!
+        Voluptate id cumque iure maxime, autem ea voluptatem aspernatur nihil
+        cum nulla repudiandae voluptatum illo, eum placeat accusantium expedita
+        perspiciatis quibusdam laboriosam sint beatae enim eveniet. Neque enim
+        ab blanditiis ut facilis, consequatur itaque saepe eius suscipit
+        possimus amet rem maxime dignissimos libero atque quod necessitatibus
+        beatae? Maiores ducimus officia ipsam excepturi sed minima velit illum
+        animi iste accusamus sit architecto deleniti laboriosam voluptas unde,
+        laudantium sapiente impedit numquam, consectetur sunt totam neque
+        pariatur. Explicabo incidunt corporis atque perspiciatis quas?
+      </p>
+    </>
+  );
+}

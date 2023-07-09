f1aa1bbad2411517f9c7e7eb5a3b5470390570a3
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-07-09 17:29:01 -0400

    Add tuiter base with initial nav

---
+++ app/tuiter/navigation/page.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <ul>
+      <li>
+        <Link href="home">Home</Link>
+      </li>
+      <li>
+        <Link href="explore">Explore</Link>
+      </li>
+      <li>
+        <Link href="notifications">Notifications</Link>
+      </li>
+      <li>
+        <Link href="messages">Messages</Link>
+      </li>
+      <li>
+        <Link href="bookmarks">Bookmarks</Link>
+      </li>
+      <li>
+        <Link href="lists">Lists</Link>
+      </li>
+      <li>
+        <Link href="profile">Profile</Link>
+      </li>
+      <li>
+        <Link href="tuit">Tuit</Link>
+      </li>
+      <li>
+        <Link href="reply">Reply</Link>
+      </li>
+    </ul>
+  );
+}

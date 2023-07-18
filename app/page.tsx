12058ca747371212a6edda6b96bf6728945891be
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-07-17 21:44:17 -0400

    Restyle landing page with Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,47 @@ import Link from "next/link";
 export default function Landing() {
   return (
     <main>
-      <h1>Web Dev Assignments</h1>
+      <h1 className="block text-4xl my-3 font-bold font-serif">
+        Web Dev Assignments
+      </h1>
 
-      <ul>
-        <li>
-          <Link href="labs/a1">Assignment 1 Labs</Link>
+      <ul className="list-disc list-inside ml-3">
+        <li className="">
+          <Link className="hover:underline text-blue-700" href="labs/a1">
+            Assignment 1 Labs
+          </Link>
         </li>
         <li>
-          <Link href="labs/a2">Assignment 2 Labs</Link>
+          <Link className="hover:underline text-blue-700" href="labs/a2">
+            Assignment 2 Labs
+          </Link>
         </li>
         <li>
-          <Link href="tuiter/navigation">Tuiter</Link>
+          <Link
+            className="hover:underline text-blue-700"
+            href="tuiter/navigation"
+          >
+            Tuiter
+          </Link>
         </li>
       </ul>
 
-      <br />
-
-      <p style={{ fontWeight: "bold" }}>Disclaimer:</p>
-      <p>
+      <p className="font-bold mt-10 font-serif">Disclaimer:</p>
+      <p className="font-serif">
         As described in Piazza Post{" "}
-        <Link href="https://piazza.com/class/ljepnjv9gy3pp/post/25">@25</Link>,
-        I have gotten permission to implement the assignments with an
+        <Link
+          className="hover:underline text-blue-700"
+          href="https://piazza.com/class/ljepnjv9gy3pp/post/25"
+        >
+          @25
+        </Link>
+        , I have gotten permission to implement the assignments with an
         alternative technology stack, so please understand the URLs, layouts,
         colors, and source code may be different. I have done my best to ensure
         the results are achieved as close to the original as possible.
       </p>
 
-      <p style={{ fontWeight: "bold" }}>Author: James Chang-Davidson</p>
+      <p className="font-bold mt-5 font-serif">Author: James Chang-Davidson</p>
     </main>
   );
 }

d64e2364051fd730c23763738f88edf0a5b0c4f8
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-07-17 23:56:14 -0400

    Even more CSS

--- app/labs/a2/page.tsx
+++ app/labs/a2/page.tsx
@@ -32,7 +32,6 @@ export default function LabA2() {
         Although it&apos;s very convenient it is considered bad practice and you
         should avoid using the style attribute
       </P>
-
       <H3>Style tag</H3>
       <P className="bg-blue-700 text-white">
         A slightly better way to configure look and feel is to declare CSS rules
@@ -40,13 +39,11 @@ export default function LabA2() {
         ID, or class This paragraph&apos;s style is set by a rule referring to
         the P tags
       </P>
-
       <H3>Link tag</H3>
       <P className="bg-blue-700 text-white">
         The best way to apply CSS rules is to declare them in a separate CSS
         file and load it with the LINK tag. Always use this method.
       </P>
-
       <H3>ID selectors</H3>
       <P id="id-selector-1" className="bg-red-500 text-white">
         Instead of changing the look and feel of all the elements of the same
@@ -56,7 +53,6 @@ export default function LabA2() {
         Here&apos;s another paragraph using a different ID and a different look
         and feel
       </P>
-
       <H3>Class selectors</H3>
       <P className="class-selector bg-yellow-300 text-blue-600">
         Instead of using IDs to refer to elements, you can use an element&apos;s
@@ -65,7 +61,6 @@ export default function LabA2() {
       <h4 className="class-selector bg-yellow-300 text-blue-600 font-serif font-bold">
         This heading has same style as paragraph above
       </h4>
-
       <div className="selector-1">
         <H3>Document structure selectors</H3>
         <div className="selector-2">
@@ -87,14 +82,12 @@ export default function LabA2() {
           </P>
         </div>
       </div>
-
       <H2>Colors</H2>
       <H3 className="text-[#7070ff]">Foreground color</H3>
       <P className="text-[#ff7070]">
         The text in this paragraph is red but{" "}
         <span className="text-green-500">this text is green</span>
       </P>
-
       <H3 className="bg-[#7070ff] text-white">Background color</H3>
       <P className=" bg-[#ff7070]  text-black">
         This background of this paragraph is red but
@@ -103,7 +96,6 @@ export default function LabA2() {
           the background of this text is green and the foreground white
         </span>
       </P>
-
       <H2>Borders</H2>
       <P className="border-x-[30px] border-y-[20px] border-[#ff7070]">
         Solid fat red border
@@ -111,31 +103,26 @@ export default function LabA2() {
       <P className="border-4 border-[#7070ff] border-dashed">
         Dashed thin blue border
       </P>
-
       <H2>Padding</H2>
       <div className="pt-[50px] pl-[50px] border-x-[30px] border-y-[20px] border-[#ff7070] bg-[#ffff07] text-center">
         Padded top left
       </div>
-
       <div className="pb-[50px] pr-[50px] border-x-[30px] border-y-[20px] border-[#7070ff] bg-[#ffff07]">
         Padded bottom right
       </div>
       <div className="p-[50px] border-x-[30px] border-y-[20px] border-[#ffff07] bg-[#7070ff] text-white">
         Padded all around
       </div>
-
       <H2>Margins</H2>
       <div className="mb-[50px] pt-[50px] pl-[50px] border-x-[30px] border-y-[20px] border-[#ff7070] bg-[#ffff07] text-center">
         Margin bottom
       </div>
-
       <div className="mx-[50px] pb-[50px] pr-[50px] border-x-[30px] border-y-[20px] border-[#7070ff] bg-[#ffff07]">
         Margin left right
       </div>
       <div className="m-[30px] p-[50px] border-x-[30px] border-y-[20px] border-[#ffff07] bg-[#7070ff] text-white">
         Margin all around
       </div>
-
       <H3>Rounded corners</H3>
       <P className="rounded-t-[40px] border-4 border-[#7070ff] p-[50px]">
         Rounded corners on the top
@@ -149,7 +136,6 @@ export default function LabA2() {
       <P className="rounded-tl-[30px] rounded-br-[20px] rounded-bl-[50px] border-4 border-[#7070ff] p-[50px]">
         Different rounded corners
       </P>
-
       <H2>Dimension</H2>
       <div>
         <div className="w-[75px] h-[100px] bg-[#ffff07]">Portrait</div>
@@ -158,6 +144,62 @@ export default function LabA2() {
         </div>
         <div className="w-[75px] h-[75px] bg-[#ff7070]">Square</div>
       </div>
+      <H2>Position</H2>
+      <H3>Relative</H3>
+      <div>
+        <div className="bg-[#ffff07] w-[75px] h-[100px]">
+          <div className="relative top-[20px] left-[20px]">Portrait</div>
+        </div>
+        <div className="relative bottom-[30px] left-[30px] bg-[#7070ff] text-white w-[100px] h-[75px]">
+          Landscape
+        </div>
+        <div className="bg-[#ff7070] w-[75px] h-[75px]">Square</div>
+      </div>
+      <H3>Absolute position</H3>
+      <div className="relative">
+        <div className="absolute top-[10px] left-[10px] w-[75px] h-[100px] bg-[#ffff07]">
+          Portrait
+        </div>
+        <div className="absolute top-[50px] left-[50px] w-[100px] h-[75px] bg-[#7070ff] text-white">
+          Landscape{" "}
+        </div>
+        <div className="absolute top-[20px] left-[120px] bg-[#ff7070] w-[75px] h-[75px]">
+          Square{" "}
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <H3>Fixed position</H3>
+      Checkout the blue square that says &quot;Fixed position&quot; stuck all
+      the way on the right and half way down the page. It doesn&apos;t scroll
+      with the rest of the page. Its position is &quot;Fixed&quot;.
+      <div className="fixed bottom-[50%] right-[0px] w-[75px] h-[75px] bg-[#7070ff] text-white">
+        Fixed position
+      </div>
+      <H2>Z index</H2>
+      <div className="relative">
+        <div className="absolute top-[10px] left-[10px]  w-[75px] h-[100px] bg-[#ffff07]">
+          Portrait
+        </div>
+        <div className="z-10 absolute top-[50px] left-[50px] w-[100px] h-[75px] bg-[#7070ff] text-white">
+          Landscape
+        </div>
+        <div className="absolute top-[20px] left-[120px] bg-[#ff7070] w-[75px] h-[75px]">
+          Square
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 }

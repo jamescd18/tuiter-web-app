401ba917a1a9f584a2d2cbd3feb2ef00789e2866
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-07-17 21:05:54 -0400

    Added assignment disclaimer

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Landing() {
   return (
     <main>
       <h1>Web Dev Assignments</h1>
+
       <ul>
         <li>
           <Link href="labs/a1">Assignment 1 Labs</Link>
@@ -12,6 +13,20 @@ export default function Landing() {
           <Link href="tuiter/navigation">Tuiter</Link>
         </li>
       </ul>
+
+      <br />
+
+      <p style={{ fontWeight: "bold" }}>Disclaimer:</p>
+      <p>
+        As described in Piazza Post{" "}
+        <Link href="https://piazza.com/class/ljepnjv9gy3pp/post/25">@25</Link>,
+        I have gotten permission to implement the assignments with an
+        alternative technology stack, so please understand the URLs, layouts,
+        and source code may be different. I have done my best to ensure the
+        results are achieved as close to the original as possible.
+      </p>
+
+      <p style={{ fontWeight: "bold" }}>Author: James Chang-Davidson</p>
     </main>
   );
 }

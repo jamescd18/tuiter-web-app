0e8d2481c89fd15b2b2260102c5c78fbdcf4c6b3
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-07-17 23:35:46 -0400

    More styling CSS

--- app/labs/a2/page.tsx
+++ app/labs/a2/page.tsx
@@ -123,6 +123,41 @@ export default function LabA2() {
       <div className="p-[50px] border-x-[30px] border-y-[20px] border-[#ffff07] bg-[#7070ff] text-white">
         Padded all around
       </div>
+
+      <H2>Margins</H2>
+      <div className="mb-[50px] pt-[50px] pl-[50px] border-x-[30px] border-y-[20px] border-[#ff7070] bg-[#ffff07] text-center">
+        Margin bottom
+      </div>
+
+      <div className="mx-[50px] pb-[50px] pr-[50px] border-x-[30px] border-y-[20px] border-[#7070ff] bg-[#ffff07]">
+        Margin left right
+      </div>
+      <div className="m-[30px] p-[50px] border-x-[30px] border-y-[20px] border-[#ffff07] bg-[#7070ff] text-white">
+        Margin all around
+      </div>
+
+      <H3>Rounded corners</H3>
+      <P className="rounded-t-[40px] border-4 border-[#7070ff] p-[50px]">
+        Rounded corners on the top
+      </P>
+      <P className="rounded-b-[40px] border-4 border-[#7070ff] p-[50px]">
+        Rounded corners at the bottom
+      </P>
+      <P className="rounded-[50px] border-4 border-[#7070ff] p-[50px]">
+        Rounded corners all around
+      </P>
+      <P className="rounded-tl-[30px] rounded-br-[20px] rounded-bl-[50px] border-4 border-[#7070ff] p-[50px]">
+        Different rounded corners
+      </P>
+
+      <H2>Dimension</H2>
+      <div>
+        <div className="w-[75px] h-[100px] bg-[#ffff07]">Portrait</div>
+        <div className="w-[100px] h-[75px] bg-[#7070ff] text-white">
+          Landscape
+        </div>
+        <div className="w-[75px] h-[75px] bg-[#ff7070]">Square</div>
+      </div>
     </>
   );
 }

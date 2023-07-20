a682fd5b74e919be044ec0108e01f9bc63453da7
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-07-20 12:41:12 -0400

    Add shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        yellow: {
-          lab: "#ffff07",
-          generic: "#ffff00",
-        },
-        blue: {
-          lab: "#7070ff",
-          generic: "#0000ff",
-        },
-        red: {
-          lab: "#ff7070",
-          generic: "#ff0000",
-        },
-        green: {
-          generic: "#00ff00",
-        },
-        bsprimary: "#0d6efd",
-        bssecondary: "#6c757d",
-        bssuccess: "#198754",
-        bsinfo: "#0dcaf0",
-        bswarning: "#ffc107",
-        bsdanger: "#dc3545",
-        bslight: "#f8f9fa",
-        bsdark: "#212529",
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
-    plugins: [],
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-};
+  plugins: [require("tailwindcss-animate")],
+}

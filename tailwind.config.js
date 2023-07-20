3da837feee197e3579d1efbdc49a6bca72b14434
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-07-20 12:58:05 -0400

    Add back colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -30,7 +30,32 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      colors: {
+        yellow: {
+          lab: "#ffff07",
+          generic: "#ffff00",
+        },
+        blue: {
+          lab: "#7070ff",
+          generic: "#0000ff",
+        },
+        red: {
+          lab: "#ff7070",
+          generic: "#ff0000",
+        },
+        green: {
+          generic: "#00ff00",
+        },
+        bsprimary: "#0d6efd",
+        bssecondary: "#6c757d",
+        bssuccess: "#198754",
+        bsinfo: "#0dcaf0",
+        bswarning: "#ffc107",
+        bsdanger: "#dc3545",
+        bslight: "#f8f9fa",
+        bsdark: "#212529",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};

8257520b391e303f9703feee91fe522a1a405e8c
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-08-05 18:23:26 -0400

    Give up

--- app/labs/a2/page.tsx
+++ app/labs/a2/page.tsx
@@ -536,6 +536,52 @@ export default function LabA2() {
         <option value="2">Two</option>
         <option value="3">Three</option>
       </select>
+      <H3>
+        <Link href="https://getbootstrap.com/docs/5.1/forms/checks-radios/#switches">
+          Switches
+        </Link>
+      </H3>
+      this is too difficult to do with tailwind
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="switch1" />
+        <label className="form-check-label" htmlFor="switch1">
+          Default switch checkbox input
+        </label>
+      </div>
+      <div className="form-check form-switch">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id="switch2"
+          checked
+        />
+        <label className="form-check-label" htmlFor="switch2">
+          Checked switch checkbox input
+        </label>
+      </div>
+      <div className="form-check form-switch">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id="switch3"
+          disabled
+        />
+        <label className="form-check-label" htmlFor="switch3">
+          Disabled switch checkbox input
+        </label>
+      </div>
+      <div className="form-check form-switch">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id="switch4"
+          checked
+          disabled
+        />
+        <label className="form-check-label" htmlFor="switch4">
+          Disabled checked switch checkbox input
+        </label>
+      </div>
     </>
   );
 }
